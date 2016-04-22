f11be289a96f0808ceb65e4668597627f04d90d2
detective7 <detective.ive@163.com>
2016-04-22 20:42:22 +0800

    but,it just views can't use the fragmentClass,,can't trigger the event

--- app/src/main/java/com/example/ys/testsms/BlankFragment.java
+++ app/src/main/java/com/example/ys/testsms/BlankFragment.java
@@ -4,6 +4,7 @@ package com.example.ys.testsms;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,10 +19,13 @@ import java.util.List;
  */
 public class BlankFragment extends Fragment {
 
+    private LayoutInflater mInflater;
+
     private ViewPager vp;
     private TabLayout mTabLayout;//标题页卡
     private List<String> mTitleList = new ArrayList<>();//页卡标题集合
-    private ArrayList<Fragment> fragments;
+    private ArrayList<View> fragments;
+    private View view1, view2, view3, view4, view5;//页卡视图
 
 
     public BlankFragment() {
@@ -33,8 +37,29 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        mInflater = LayoutInflater.from(getActivity());
+
         //获取标题页卡实例
         mTabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        fragments = new ArrayList<View>();
+        view1 = mInflater.inflate(R.layout.fl1, null);
+        view2 = mInflater.inflate(R.layout.fragment_blank, null);
+        view3 = mInflater.inflate(R.layout.fragment_blank, null);
+        view4 = mInflater.inflate(R.layout.fragment_blank, null);
+        view5 = mInflater.inflate(R.layout.fragment_blank, null);
+        //添加页卡视图
+        fragments.add(view1);
+        fragments.add(view2);
+        fragments.add(view3);
+        fragments.add(view4);
+        fragments.add(view5);
+
+//        fragments.add(new fl1());
+//        fragments.add(new fl2());
+//        fragments.add(new fl3());
+//        fragments.add(new fl1());
+//        fragments.add(new fl2());
+
 
         //添加页卡标题
         mTitleList.add("No:1");
@@ -50,13 +75,7 @@ public class BlankFragment extends Fragment {
         mTabLayout.addTab(mTabLayout.newTab().setText(mTitleList.get(3)));
         mTabLayout.addTab(mTabLayout.newTab().setText(mTitleList.get(4)));
 
-        //构造适配器
-        fragments = new ArrayList<Fragment>();
-        fragments.add(new fl1());
-        fragments.add(new fl2());
-        fragments.add(new fl3());
-
-        FragAdapter adapter = new FragAdapter(this.getChildFragmentManager(), fragments);
+        MyPagerAdapter adapter = new MyPagerAdapter(fragments);
 
         //设定适配器
         vp = (ViewPager) view.findViewById(R.id.viewpager);
@@ -68,4 +87,38 @@ public class BlankFragment extends Fragment {
         return view;
     }
 
+    private class MyPagerAdapter extends PagerAdapter {
+        private List<View> mViewList;
+
+        public MyPagerAdapter(List<View> mViewList) {
+            this.mViewList = mViewList;
+        }
+
+        @Override
+        public int getCount() {
+            return mViewList.size();//页卡数
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;//官方推荐写法
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mViewList.get(position));//添加页卡
+            return mViewList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(mViewList.get(position));//删除页卡
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitleList.get(position);//页卡标题
+        }
+
+    }
 }
--- app/src/main/java/com/example/ys/testsms/fl1.java
+++ app/src/main/java/com/example/ys/testsms/fl1.java
@@ -6,13 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class fl1 extends Fragment {
-
+    EditText et1;
 
     public fl1() {
         // Required empty public constructor
@@ -23,6 +26,14 @@ public class fl1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fl1, container, false);
+        et1 = (EditText) view.findViewById(R.id.editText);
+        Button bt1 = (Button) view.findViewById(R.id.button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), et1.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
